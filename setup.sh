786c08d3b57bcc89abe7a0ed4650ee93dea6add3
Ubuntu <ubuntu@ip-172-31-36-118.eu-west-1.compute.internal>
2025-03-17 09:52:25 +0000

    Adding setup.sh file

--- setup.sh
+++ setup.sh
@@ -3,3 +3,4 @@ source env/bin/activate
 pip install dvc
 pip install "dvc[s3]"
 pip install -r ./requirements.txt
+wget -O data/raw_data/raw.csv https://datascientest-mlops.s3.eu-west-1.amazonaws.com/mlops_dvc_fr/raw.csv
